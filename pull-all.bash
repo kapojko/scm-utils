727498e4365dde6daac0d3f92cfaf10e49b978ff
Yuriy Kapoyko <yury.kapoyko@gmail.com>
2024-05-21 11:27:40 +0300

    Added pull-all bash/ps1 script

---
+++ pull-all.bash
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+
+SAVEIFS=$IFS
+IFS=$(echo -en "\n\b")
+
+function process_dir() {
+    local PREV_DIR=`pwd`
+    cd $1
+
+    if test -d ".svn"
+    then
+        echo "==========" `pwd` "=========="
+        #svn st
+        echo "Not implemented!"
+        exit 1
+    elif test -d ".git"
+    then
+        echo "==========" `pwd` "=========="
+        git pull
+    elif test -d ".bzr"
+    then
+        echo "==========" `pwd` "=========="
+        #bzr st
+        echo "Not implemented!"
+        exit 1
+    elif test -d ".hg"
+    then
+        echo "==========" `pwd` "=========="
+        #hg st
+        echo "Not implemented!"
+        exit 1
+    else
+        for SUB_DIR in $(find . -maxdepth 1 -mindepth 1 -type d)
+        do
+            process_dir "$SUB_DIR"
+        done
+    fi
+
+    cd $PREV_DIR
+}
+
+process_dir .
+
+IFS=$SAVEIFS
+
